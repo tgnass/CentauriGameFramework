ae45a1142e6a421e5d5a0711934be367cf0022b1
tgnass <tgnass@gmail.com>
2019-05-05 13:26:40 -0400

    Added more prefixes for Util:ReadableNumber()

--- src/ReplicatedStorage/Centauri/Shared/Util.lua
+++ src/ReplicatedStorage/Centauri/Shared/Util.lua
@@ -45,8 +45,16 @@ function module:ReadableNumber(number, places, canAddCommas)
 	end
 	if not number then
 		return 0
-	elseif number >= 1000000000000 then
-		returnValue = placeValue:format(number / 1000000000000):gsub("%.?0+$", "") .. " T"
+	elseif number >= 1e+24 then
+		returnValue = placeValue:format(number / 1e+24):gsub("%.?0+$", "") .. " Y"
+	elseif number >= 1e+21 then
+		returnValue = placeValue:format(number / 1e+21):gsub("%.?0+$", "") .. " Z"
+	elseif number >= 1e+18 then
+		returnValue = placeValue:format(number / 1e+18):gsub("%.?0+$", "") .. " E"
+	elseif number >= 1e+15 then
+		returnValue = placeValue:format(number / 1e+15):gsub("%.?0+$", "") .. " P"
+	elseif number >= 1e+12 then
+		returnValue = placeValue:format(number / 1e+12):gsub("%.?0+$", "") .. " T"
 	elseif number >= 1000000000  then
 		returnValue = placeValue:format(number / 1000000000):gsub("%.?0+$", "") .. " B"
 	elseif number >= 1000000 then
